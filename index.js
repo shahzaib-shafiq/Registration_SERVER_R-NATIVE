67a630dca2f8fbe26ed7f3ab38baca6a10ad655c
Shahzaib Shafiq <73356944+shahzaibshafiq100@users.noreply.github.com>
2023-11-09 17:57:43 +0500

    init

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const expres=require('express') ;
-const post=3000;
-
-
-const app=expres();
+const express = require('express')
+const app = express()
+const port = 3000
 const bodyParser=require('body-parser');
 
 app.use(bodyParser.json());
 
 
-app.get('/',(req,res)=>{
-    res.send('fa')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.post('/signup', (req, res) => {
+    res.send('POST request to the homepage')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
